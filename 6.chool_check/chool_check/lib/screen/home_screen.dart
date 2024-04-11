6b714675939e7c39226c2a5dc3d9906a5e5f5ca1
jsyoon <younjung1996@gmail.com>
2024-04-11 23:33:38 +0900

    gps test
    gps test

--- 6.chool_check/chool_check/lib/screen/home_screen.dart
+++ 6.chool_check/chool_check/lib/screen/home_screen.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -19,14 +22,51 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: renderAppBar(),
-      body: Column(
-        children: [
-          _CustomGoogleMap(initialPosition: initialPosition),
-          _ChoolCheckButton(),
-        ],
-      ),
-    );
+        appBar: renderAppBar(),
+        body: FutureBuilder<String>(
+            future: checkPermission(),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+
+              if (snapshot.data == '위치 권한이 허가되었습니다.') {
+                return Column(
+                  children: [
+                    _CustomGoogleMap(initialPosition: initialPosition),
+                    _ChoolCheckButton(),
+                  ],
+                );
+              } else {
+                return Center(child: Text(snapshot.data));
+              }
+            }));
+  }
+
+  Future<String> checkPermission() async {
+    final isLocationEnabled = await Geolocator.isLocationServiceEnabled();
+
+    if (!isLocationEnabled) {
+      return '위치 서비스를 활성화 해주세요.';
+    }
+
+    LocationPermission checkedPermission = await Geolocator.checkPermission();
+
+    if (checkedPermission == LocationPermission.denied) {
+      checkedPermission = await Geolocator.requestPermission();
+
+      if (checkedPermission == LocationPermission.denied) {
+        return '위치 권한을 허가해주세요.';
+      }
+    }
+
+    if (checkedPermission == LocationPermission.deniedForever) {
+      return '앱의 위치 권한을 세팅에서 허가해주세요.';
+    }
+
+    return '위치 권한이 허가되었습니다.';
   }
 
   AppBar renderAppBar() {
@@ -51,6 +91,7 @@ class _CustomGoogleMap extends StatelessWidget {
       child: GoogleMap(
         mapType: MapType.normal,
         initialCameraPosition: initialPosition,
+        myLocationEnabled: true,
       ),
     );
   }
